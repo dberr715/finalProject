36e7f87a642a0edec6281ab724af6bec066b880c
David Berry <dberry3755@gmail.com>
2023-10-27 16:51:37 -0400

    working user database in postgres and render

--- backend_final_project/footyMatchApp/models.py
+++ backend_final_project/footyMatchApp/models.py
@@ -6,6 +6,7 @@ class User(models.Model):
     username = models.CharField()
     password = models.CharField()
     token = models.CharField()
+    favorites = models.CharField(default="No Favorites Set")
 
     def __str__(self):
         return self.username
