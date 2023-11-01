6901f90a964d5f1d4ff4e3da7b4534b6f62439ca
David Berry <dberry3755@gmail.com>
2023-11-01 14:55:21 -0400

    search bar within modal now routing to correct page with team name as well

--- frontend_final_project/frontend_footymatch/src/components/HomeText.jsx
+++ frontend_final_project/frontend_footymatch/src/components/HomeText.jsx
@@ -1,7 +1,10 @@
+import "../index.css";
+import AIresponse from "./AIResponse";
 import React, { useState } from "react";
-import AIResponse from "./AIResponse";
+import { useNavigate } from "react-router-dom";
 
 export default function HomeText() {
+  const navigate = useNavigate();
   const [searchedTeam, setSearchedTeam] = useState("");
 
   const handleSearch = (team) => {
@@ -9,22 +12,28 @@ export default function HomeText() {
   };
 
   const [team, setTeam] = useState("");
-
   const handleInputChange = (e) => {
     setTeam(e.target.value);
   };
 
   const handleSearchClick = () => {
     handleSearch(team);
+    navigate(`/team/${team}`, { team });
   };
 
   return (
     <>
       <h1>Welcome to FootyMatch!</h1>
 
+      {/* <form>
+        <label>
+          <input type="text" placeholder="🏀🏈⚾️🎾" />
+          <button type="submit">Match!</button>
+        </label>
+      </form> */}
       <h4>⚽️Find your new soccer team to support⚽️</h4>
       <p>Type 1 or more sports teams in to find your soccer team match! </p>
-      <AIResponse handleSearch={handleSearch} />
+      <AIresponse />
       <br />
       <h4>Already have a team?</h4>
       <p>Search for it below!</p>
--- frontend_final_project/frontend_footymatch/src/components/ResponseModal.jsx
+++ frontend_final_project/frontend_footymatch/src/components/ResponseModal.jsx
@@ -1,31 +1,80 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function ResponseModal({ rec, onClose, handleSearch }) {
-  const [selectedTeam, setSelectedTeam] = useState("");
+export default function ResponseModal({ rec, onClose }) {
+  const navigate = useNavigate();
+  const [team, setTeam] = useState("");
+  const [teamName, setTeamName] = useState("");
+  const [logo, setLogo] = useState("");
+  const [country, setCountry] = useState("");
+  const [stadium, setStadium] = useState("");
+  const [stadiumPic, setStadiumPic] = useState("");
+  const [error, setError] = useState(null);
 
-  const searchForTeam = () => {
-    if (selectedTeam) {
-      handleSearch(selectedTeam);
+  const handleInputChange = (e) => {
+    setTeam(e.target.value);
+  };
+
+  const handleSearchClick = async () => {
+    // API fetch logic
+    const key = import.meta.env.VITE_FOOTBALL_API_KEY;
+    const nameUrl = `https://api-football-v1.p.rapidapi.com/v3/teams?name=${team}`;
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": key,
+        "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await fetch(nameUrl, options);
+
+      if (response.ok) {
+        const result = await response.json();
+        const name = result.response[0].team.name;
+        const logo = result.response[0].team.logo;
+        const stadium = result.response[0].venue.name;
+        const stadiumPic = result.response[0].venue.image;
+        const country = result.response[0].team.country;
+        setTeamName(name);
+        setLogo(logo);
+        setStadium(stadium);
+        setStadiumPic(stadiumPic);
+        setCountry(country);
+
+        navigate(`/team/${team}`);
+      } else {
+        setError("Team not found. Please try again.");
+      }
+    } catch (error) {
+      setError(
+        "That is not a professional soccer team. Please check your spelling and try again."
+      );
     }
   };
+
   return (
     <div className="modal">
       <div className="modal-content">
-        <div className="modal-header">
-          <span className="close" onClick={onClose}>
-            &times;
-          </span>
-        </div>
-        <div className="modal-body">
-          <p>{rec}</p>
-          <input
-            type="text"
-            placeholder="Enter team here"
-            value={selectedTeam}
-            onChange={(e) => setSelectedTeam(e.target.value)}
-          />
-          <button onClick={searchForTeam}>Search</button>
-        </div>
+        <span className="close" onClick={onClose}>
+          &times;
+        </span>
+        <p>{rec}</p>
+
+        <input
+          type="text"
+          value={team}
+          onChange={handleInputChange}
+          placeholder="🔍"
+        />
+        <button type="submit" onClick={handleSearchClick}>
+          Search
+        </button>
+        {error && (
+          // Display the error message if an error occurred
+          <p className="error-message">{error}</p>
+        )}
       </div>
     </div>
   );
