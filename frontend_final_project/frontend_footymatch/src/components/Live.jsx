15ceb43a15fb6ae3456e0a8bfd9b1d7c4fc9fa2a
David Berry <dberry3755@gmail.com>
2023-11-01 17:34:11 -0400

    live scores working, styled more with flex boxes

--- frontend_final_project/frontend_footymatch/src/components/Live.jsx
+++ frontend_final_project/frontend_footymatch/src/components/Live.jsx
@@ -6,13 +6,13 @@ export default function Live() {
 
   useEffect(() => {
     const key = import.meta.env.VITE_FOOTBALL_API_KEY;
+
     const fetchLiveScores = async () => {
       const url = "https://api-football-v1.p.rapidapi.com/v3/fixtures?live=all";
       const options = {
         method: "GET",
         headers: {
-          "X-RapidAPI-Key":
-            key,
+          "X-RapidAPI-Key": key,
           "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
         },
       };
@@ -21,8 +21,15 @@ export default function Live() {
         const response = await fetch(url, options);
         const data = await response.json();
 
-        // Filter the live games to get the first 10 elements
-        setLiveScores(data.response.slice(0, 10));
+        // Sort live games by elapsed time in descending order
+        const sortedLiveScores = data.response.sort(
+          (a, b) => b.fixture.elapsed - a.fixture.elapsed
+        );
+
+        // Select the 10 games with the lowest elapsed time (closest to the end of the game)
+        const closestToTheEnd = sortedLiveScores.slice(0, 10);
+
+        setLiveScores(closestToTheEnd);
       } catch (error) {
         console.error(error);
       }
@@ -34,22 +41,47 @@ export default function Live() {
   return (
     <div>
       <h1>Live Scores</h1>
-      <ul>
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
         {liveScores.map((score) => (
-          <li key={score.fixture.id}>
-            <Link to={`/game/${score.fixture.id}`}>
-              <img src={score.teams.home.logo} alt={score.teams.home.name} />
-              {score.teams.home.name} {score.goals.home} - {score.goals.away}{" "}
-              {score.teams.away.name}
-              <img src={score.teams.away.logo} alt={score.teams.away.name} />
-              <br />
-              <br />
-              <br />
-              <br />
+          <div
+            key={score.fixture.id}
+            style={{
+              border: "1px solid black",
+              margin: "10px",
+              padding: "10px",
+              width: "500px",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              textAlign: "center",
+            }}
+          >
+            <Link
+              to={`/game/${score.fixture.id}`}
+              style={{
+                textDecoration: "underline",
+                color: "black", // Keep the text color black
+              }}
+            >
+              <img
+                src={score.teams.home.logo}
+                alt={score.teams.home.name}
+                style={{ width: "75px", height: "75px" }}
+              />
+              <div style={{ fontSize: "24px" }}>
+                {score.teams.home.name} {score.goals.home} - {score.goals.away}{" "}
+                {score.teams.away.name}
+              </div>
+              <img
+                src={score.teams.away.logo}
+                alt={score.teams.away.name}
+                style={{ width: "75px", height: "75px" }}
+              />
+              <p style={{ fontSize: "18px" }}></p>
             </Link>
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
