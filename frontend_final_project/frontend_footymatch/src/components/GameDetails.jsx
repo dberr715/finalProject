6f73f57320ca7e644d90fb111fee8f1d721a7413
David Berry <dberry3755@gmail.com>
2023-11-13 11:49:30 -0500

    updated scores to be in correct spot

--- frontend_final_project/frontend_footymatch/src/components/GameDetails.jsx
+++ frontend_final_project/frontend_footymatch/src/components/GameDetails.jsx
@@ -11,6 +11,9 @@ export default function GameDetails() {
   const [awayLogo, setAwayLogo] = useState(null);
   const [homeTeamName, setHomeTeamName] = useState(null);
   const [awayTeamName, setAwayTeamName] = useState(null);
+  const [homeScore, setHomeScore] = useState(null);
+  const [awayScore, setAwayScore] = useState(null);
+//   const [totalScore, setTotalScore] = useState(null);
 
   const navigate = useNavigate();
 
@@ -63,6 +66,17 @@ export default function GameDetails() {
             (event) => event.team.name === awayTeamName
           );
 
+          // Find home and away scores
+          const homeScore = homeEvents.filter(
+            (event) => event.type === "Goal"
+          ).length;
+          const awayScore = awayEvents.filter(
+            (event) => event.type === "Goal"
+          ).length;
+
+          setHomeScore(homeScore);
+          setAwayScore(awayScore);
+
           // Set home and away team events
           setHomeTeamEvents(homeEvents);
           setAwayTeamEvents(awayEvents);
@@ -82,148 +96,155 @@ export default function GameDetails() {
     navigate(-1);
   };
 
-  return (
-    <>
-      <Navigation />
-      <div className="card1 card3">
-        <button
-          className="back-button more-info-button"
-          onClick={handleBackButtonClick}
-        >
-          Back
-        </button>
-        {fixtureDetails && (
-          <div className="soccer-scoring-card">
-            <h1 className="league-name">Match Events</h1>
-            <div className="teams-container">
-              {/* Swap the order of the teams here */}
-              <div className="team">
-                <div className="team-logo">
-                  <img src={awayLogo} alt="away team logo" />
-                </div>
-                <div className="team-details">
-                  <h2 className="team-name">{awayTeamName}</h2>
-                </div>
-                <div className="events-container">
-                  <div className="team-score">Score: </div>
-                  {awayTeamEvents.length === 0 && (
-                    <div className="placeholder-message">
-                      No match events yet
-                    </div>
-                  )}
-                  {awayTeamEvents.map((event, index) => (
-                    <div key={index} className="match-event">
-                      <div className="event-details">
-                        <div className="event-time smaller ">
-                          {event.time.elapsed} min &nbsp;
-                        </div>
-                        <div className="event-type-icon">
-                          {event.detail === "Red Card" && (
-                            <img
-                              src="../../public/redcard.png"
-                              alt="Red Card"
-                              className="small-icon"
-                            />
-                          )}
-                          {event.detail === "Yellow Card" && (
-                            <img
-                              src="../../public/yellowcard.png"
-                              alt="Yellow Card"
-                              className="small-icon"
-                            />
-                          )}
-                          {event.detail.startsWith("Substitution") && (
-                            <img
-                              src="../../public/substitution.png"
-                              alt={`Substitution ${event.detail.charAt(13)}`}
-                              className="small-icon"
-                            />
-                          )}
-                          {event.detail === "Normal Goal" && (
-                            <img
-                              src="../../public/soccerballicon.png"
-                              alt="Goal"
-                              className="small-icon"
-                            />
-                          )}
-                        </div>
-                        <div className="event-type smaller">
-                          &nbsp; {event.type} &nbsp;
-                        </div>
-                        <div className="player-name smaller bold">
-                          &nbsp;{event.player.name} &nbsp;{" "}
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-              <div className="vertical-line"></div>
-              <div className="team">
-                <div className="team-logo">
-                  <img src={homeLogo} alt="home Team Logo" />
-                </div>
-                <div className="team-details">
-                  <h2 className="team-name">{homeTeamName}</h2>
-                </div>
-                <div className="events-container">
-                  <div className="team-score">Score: </div>
-                  {homeTeamEvents.length === 0 && (
-                    <div className="placeholder-message">
-                      No match events yet
-                    </div>
-                  )}
-                  {homeTeamEvents.map((event, index) => (
-                    <div key={index} className="match-event">
-                      <div className="event-details">
-                        <div className="event-time smaller ">
-                          {event.time.elapsed} min &nbsp;
-                        </div>
-                        <div className="event-type-icon">
-                          {event.detail === "Red Card" && (
-                            <img
-                              src="../../public/redcard.png"
-                              alt="Red Card"
-                              className="small-icon"
-                            />
-                          )}
-                          {event.detail === "Yellow Card" && (
-                            <img
-                              src="../../public/yellowcard.png"
-                              alt="Yellow Card"
-                              className="small-icon"
-                            />
-                          )}
-                          {event.detail.startsWith("Substitution") && (
-                            <img
-                              src="../../public/substitution.png"
-                              alt={`Substitution ${event.detail.charAt(13)}`}
-                              className="small-icon"
-                            />
-                          )}
-                          {event.detail === "Normal Goal" && (
-                            <img
-                              src="../../public/soccerballicon.png"
-                              alt="Goal"
-                              className="small-icon"
-                            />
-                          )}
-                        </div>
-                        <div className="event-type smaller">
-                          &nbsp;{event.type}&nbsp;
-                        </div>
-                        <div className="player-name smaller bold">
-                          &nbsp; {event.player.name} &nbsp;
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </div>
-        )}
-      </div>
-    </>
-  );
+   return (
+     <>
+       <Navigation />
+       <div className="card1 card3">
+         <button
+           className="back-button more-info-button"
+           onClick={handleBackButtonClick}
+         >
+           Back
+         </button>
+         {fixtureDetails && (
+           <div className="soccer-scoring-card">
+             <h1 className="league-name">Match Events</h1>
+             <div className="teams-container">
+               <div className="team">
+                 <div className="team-logo">
+                   <img src={awayLogo} alt="away team logo" />
+                 </div>
+                 <div className="team-details">
+                   <h2 className="team-name">{awayTeamName}</h2>
+                 </div>
+                 <div className="events-container">
+                   {awayTeamEvents.length === 0 && (
+                     <div className="placeholder-message">
+                       No match events yet
+                     </div>
+                   )}
+                   {awayTeamEvents.map((event, index) => (
+                     <div key={index} className="match-event">
+                       <div className="event-details">
+                         <div className="event-time smaller ">
+                           {event.time.elapsed} min &nbsp;
+                         </div>
+                         <div className="event-type-icon">
+                           {event.detail === "Red Card" && (
+                             <img
+                               src="../../public/redcard.png"
+                               alt="Red Card"
+                               className="small-icon"
+                             />
+                           )}
+                           {event.detail === "Yellow Card" && (
+                             <img
+                               src="../../public/yellowcard.png"
+                               alt="Yellow Card"
+                               className="small-icon"
+                             />
+                           )}
+                           {event.detail.startsWith("Substitution") && (
+                             <img
+                               src="../../public/substitution.png"
+                               alt={`Substitution ${event.detail.charAt(13)}`}
+                               className="small-icon"
+                             />
+                           )}
+                           {event.detail === "Normal Goal" && (
+                             <img
+                               src="../../public/soccerballicon.png"
+                               alt="Goal"
+                               className="small-icon"
+                             />
+                           )}
+                         </div>
+                         <div className="event-type smaller">
+                           &nbsp; {event.type} &nbsp;
+                         </div>
+                         <div className="player-name smaller bold">
+                           &nbsp;{event.player.name} &nbsp;{" "}
+                         </div>
+                       </div>
+                     </div>
+                   ))}
+                 </div>
+               </div>
+               <div className="vertical-line"></div>
+               <div className="score-column">
+                 <div className="match-score">
+                   <span className="match-score-number match-score-number--leading">
+                     {homeScore}
+                   </span>
+                   <span className="match-score-divider">:</span>
+                   <span className="match-score-number">{awayScore}</span>
+                 </div>
+               </div>
+               <div className="vertical-line"></div>
+               <div className="team">
+                 <div className="team-logo">
+                   <img src={homeLogo} alt="home Team Logo" />
+                 </div>
+                 <div className="team-details">
+                   <h2 className="team-name">{homeTeamName}</h2>
+                 </div>
+                 <div className="events-container">
+                   {homeTeamEvents.length === 0 && (
+                     <div className="placeholder-message">
+                       No match events yet
+                     </div>
+                   )}
+                   {homeTeamEvents.map((event, index) => (
+                     <div key={index} className="match-event">
+                       <div className="event-details">
+                         <div className="event-time smaller ">
+                           {event.time.elapsed} min &nbsp;
+                         </div>
+                         <div className="event-type-icon">
+                           {event.detail === "Red Card" && (
+                             <img
+                               src="../../public/redcard.png"
+                               alt="Red Card"
+                               className="small-icon"
+                             />
+                           )}
+                           {event.detail === "Yellow Card" && (
+                             <img
+                               src="../../public/yellowcard.png"
+                               alt="Yellow Card"
+                               className="small-icon"
+                             />
+                           )}
+                           {event.detail.startsWith("Substitution") && (
+                             <img
+                               src="../../public/substitution.png"
+                               alt={`Substitution ${event.detail.charAt(13)}`}
+                               className="small-icon"
+                             />
+                           )}
+                           {event.detail === "Normal Goal" && (
+                             <img
+                               src="../../public/soccerballicon.png"
+                               alt="Goal"
+                               className="small-icon"
+                             />
+                           )}
+                         </div>
+                         <div className="event-type smaller">
+                           &nbsp;{event.type}&nbsp;
+                         </div>
+                         <div className="player-name smaller bold">
+                           &nbsp; {event.player.name} &nbsp;
+                         </div>
+                       </div>
+                     </div>
+                   ))}
+                 </div>
+               </div>
+             </div>
+           </div>
+         )}
+       </div>
+     </>
+   );
 }
