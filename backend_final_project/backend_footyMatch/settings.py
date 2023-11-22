3a3623187c308e3febef259bb66987607c769bc7
David Berry <dberry3755@gmail.com>
2023-11-22 11:25:38 -0500

    commented out all auth middlwares

--- backend_final_project/backend_footyMatch/settings.py
+++ backend_final_project/backend_footyMatch/settings.py
@@ -46,7 +46,7 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "allauth.account.middleware.AuthenticationMiddleware",
+    # "allauth.account.middleware.AuthenticationMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     # "allauth.account.middleware.AccountMiddleware",
