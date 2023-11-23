cd28a9257f227d4f4f4b627dcd2b9b3be48b55f4
David Berry <dberry3755@gmail.com>
2023-11-23 07:46:52 -0500

    updated allowed hosts to render site

--- backend_final_project/backend_footyMatch/settings.py
+++ backend_final_project/backend_footyMatch/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = env("SECRET_KEY")
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "footymatch1.onrender.com"]
 CORS_ORIGIN_ALLOW_ALL = True
 
 
